138d9cd5a0279d5ed87ee987138ef35de8a6a17b
Nareauphol Liu <nareauphol.liu@berkeley.edu>
2021-11-13 15:06:49 -0800

    add bluetooth

--- bluetooth.py
+++ bluetooth.py
@@ -1,4 +1,16 @@
-import serial
-from bluetooth import *
-nearby_devices = discover_devices(lookup_names = True)
-print(nearby_devices)
+"""
+A simple Python script to send messages to a sever over Bluetooth using
+Python sockets (with Python 3.3 or above).
+"""
+
+import socket
+serverMACAddress = '68:9a:87:ca:c3:08'
+port = "/dev/cu.usbserial-014AD0FD"
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((serverMACAddress,port))
+while 1:
+    text = input()
+    if text == "quit":
+        break
+    s.send(bytes(text, 'UTF-8'))
+s.close()
