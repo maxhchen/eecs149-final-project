1745ad2371e872fff599e210042a1a5d5041d8bd
Maxwell Chen <maxhchen@gmail.com>
2021-11-15 23:17:31 -0800

    Clean up and comment code

--- bluetooth_test.ino
+++ bluetooth_test.ino
@@ -5,10 +5,13 @@
 uint8_t addrs[7] = {0};
 uint8_t device_count = 0;
 
+// Scans for connection to MPU 9250 over I2C.
+// Helper function from https://github.com/hideakitai/MPU9250.
 template <typename WireType = TwoWire>
 void scan_mpu(WireType& wire = Wire) {
   Serial.println("Searching for i2c devices...");
   device_count = 0;
+
   for (uint8_t i = 0x68; i < 0x70; ++i) {
     wire.beginTransmission(i);
     if (wire.endTransmission() == 0) {
@@ -16,195 +19,100 @@ void scan_mpu(WireType& wire = Wire) {
       delay(10);
     }
   }
-  Serial.print("Found ");
-  Serial.print(device_count, DEC);
-  Serial.println(" I2C devices");
+
+  Serial.print("Found "); Serial.print(device_count, DEC); Serial.println(" I2C devices");
 
   Serial.print("I2C addresses are: ");
   for (uint8_t i = 0; i < device_count; ++i) {
-    Serial.print("0x");
-    Serial.print(addrs[i], HEX);
-    Serial.print(" ");
+    Serial.print("0x"); Serial.print(addrs[i], HEX); Serial.print(" ");
   }
   Serial.println();
 }
-
-template <typename WireType = TwoWire>
-uint8_t readByte(uint8_t address, uint8_t subAddress, WireType& wire = Wire) {
-  uint8_t data = 0;
-  wire.beginTransmission(address);
-  wire.write(subAddress);
-  wire.endTransmission(false);
-  wire.requestFrom(address, (size_t)1);
-  if (wire.available()) data = wire.read();
-  return data;
-}
-
-void printDeviceAddress() {
-
-  const uint8_t* point = esp_bt_dev_get_address();
-
-  for (int i = 0; i < 6; i++) {
-
-    char str[3];
-
-    sprintf(str, "%02X", (int)point[i]);
-    Serial.print(str);
-
-    if (i < 5) {
-      Serial.print(":");
-    }
-
-  }
-}
-
-
 
 // https://makeabilitylab.github.io/physcomp/esp32/esp32.html
 // ADC#1 is the only ADC that works when using WiFi
 // ADC#2 only works when WiFi has not started.
 // Nov 13th: Checked ADC on all pins -> Worked as expected
+
+// Note: Serial   --> ESP32
+//       SerialBT --> Computer
 BluetoothSerial SerialBT;
 MPU9250 mpu;
-const int pin14 = 14;
-const int pin15 = 15;
-const int pin13 = 13;
-const int pin12 = 12;
+// const int pin14 = 14;
+// const int pin15 = 15;
+// const int pin13 = 13;
+// const int pin12 = 12;
+
 void setup() {
   // Setup Bluetooth
-  Serial.begin(115200);
-  SerialBT.begin("ESP32 EECS149");
-  Serial.println("The device started, now you can pair it with bluetooth!");
-
+  Serial.begin(/*Baud Rate = */ 115200);
+  SerialBT.begin(/*Device Name = */ "ESP32 EECS149");
+  Serial.println("The device has started -- ready to pair with bluetooth!");
 
   // Join I2C bus with IMU (I2Cdev library doesn't do this automatically)
   Wire.begin();
   delay(2000);
 
-  // Calibrate MPU https://github.com/hideakitai/MPU9250
+  // Calibrate MPU (https://github.com/hideakitai/MPU9250)
   scan_mpu();
 
   if (device_count == 0) {
-    Serial.println("No device found on I2C bus. Please check your hardware connection");
-    while (1);
+    Serial.println("No device found on I2C bus. Please check your hardware connection.");
+    while (1); // TODO: Change to return or throw error?
   }
 
   for (uint8_t i = 0; i < device_count; ++i) {
-    Serial.print("I2C address 0x");
-    Serial.print(addrs[i], HEX);
-    byte ca = readByte(addrs[i], WHO_AM_I_MPU9250);
-    if (ca == MPU9250_WHOAMI_DEFAULT_VALUE) {
-      Serial.println(" is MPU9250 and ready to use");
-    } else if (ca == MPU9255_WHOAMI_DEFAULT_VALUE) {
-      Serial.println(" is MPU9255 and ready to use");
-    } else if (ca == MPU6500_WHOAMI_DEFAULT_VALUE) {
-      Serial.println(" is MPU6500 and ready to use");
-    } else {
-      Serial.println(" is not MPU series");
-      Serial.print("WHO_AM_I is ");
-      Serial.println(ca, HEX);
-      Serial.println("Please use correct device");
-    }
+    Serial.print("I2C address 0x"); Serial.println(addrs[i], HEX);
   }
 
-  Serial.print("Found ");
-  Serial.print(device_count, DEC);
-  Serial.println(" I2C devices");
+  Serial.print("Found "); Serial.print(device_count, DEC); Serial.println(" I2C devices.");
 
-  mpu.setup(0x68);
+  if (!mpu.setup(0x68)) {
+    Serial.println("Could not connect to MPU. Please check your hardware connection.");
+    while (1); // TODO: Change to return or throw error?
+  }
 
   // Start Calibration 
-  //  Serial.println("Accel Gyro calibration will start in 5sec.");
-  //  Serial.println("Please leave the device still on the flat plane.");
-  //  mpu.verbose(true);
-  //  delay(5000);
-  //  mpu.calibrateAccelGyro();
-  //
-  //  Serial.println("Mag calibration will start in 5sec.");
-  //  Serial.println("Please Wave device in a figure eight until done.");
-  //  delay(5000);
-  //  mpu.calibrateMag();
-  //
-  //  print_calibration();
-  //  mpu.verbose(false);
+   Serial.println("Starting Accelerometer/Gyroscope calibration in 5s...");
+   Serial.println("Please leave your device still on a flat plane.");
+   delay(5000);
+   mpu.calibrateAccelGyro();
+   Serial.println("Accelerometer/Gyroscope calibration finished!");
+  
+   Serial.println("Starting Magnetometer calibration in 5s...");
+   Serial.println("Please wave your device in a figure-eight until done.");
+   delay(5000);
+   mpu.calibrateMag();
+   Serial.println("Magnetometer calibration finished!");
+  
+   print_calibration();
 }
 
 
-// can only write a maximum of 8 bit integer
+// Can only write a maximum of 8-bit integer values
 void loop() {
-  //    if (mpu.update()) {
-  //        Serial.print(mpu.getYaw()); Serial.print(", ");
-  //        Serial.print(mpu.getPitch()); Serial.print(", ");
-  //        Serial.println(mpu.getRoll());
-  //    }
-
-
   if (mpu.update()) {
-    Serial.print("Accel: "); Serial.print(mpu.getAccX()); Serial.print(", ");
+    // Echo sensor data to ESP32 monitor
+    Serial.print("Accel: ");
+    Serial.print(mpu.getAccX()); Serial.print(", ");
     Serial.print(mpu.getAccY()); Serial.print(", ");
     Serial.println(mpu.getAccZ());
+
+    // "SerialBT" will actually send sensor data to computer
     SerialBT.print(mpu.getAccX());
-    SerialBT.print("-");
-//    Serial.print("Gyro: "); Serial.print(mpu.getGyroX()); Serial.print(", ");
-//    Serial.print(mpu.getGyroY()); Serial.print(", ");
-//    Serial.println(mpu.getGyroZ());
 
-    //float getMagX() const;
-    //float getMagY() const;
-    //float getMagZ() const;
+    // Delimiter to separate values when parsing data on computer
+    SerialBT.print("|");
 
+    // Serial.print("Gyro: ");
+    // Serial.print(mpu.getGyroX()); Serial.print(", ");
+    // Serial.print(mpu.getGyroY()); Serial.print(", ");
+    // Serial.println(mpu.getGyroZ());
+
+    // Serial.print("Mag: ");
+    // Serial.print(mpu.getMagX()); Serial.print(", ");
+    // Serial.print(mpu.getMagY()); Serial.print(", ");
+    // Serial.println(mpu.getMagZ());
   }
-
-
-  //  Serial.println(analogRead(pin12));
-  // SerialBT.write(200);
-  //  SerialBT.println(Serial.available());
-  //  SerialBT.println(SerialBT.available());
-  //  SerialBT.println(46);
-  //  SerialBT.println("----");
-
-  // Reads from the arduino serial and writes it to computer via bluetooth
-  //  if (Serial.available()) {
-  //    SerialBT.write(40);
-  //    SerialBT.write(Serial.read());
-  //  }
-  //
-  //  // Reads from the computer via bluetooth and writes it to the Arduino serial
-  //  if (SerialBT.available()) {
-  //    SerialBT.write(50);
-  //    Serial.write(SerialBT.read());
-  //  }
   delay(10);
-}
-
-void print_calibration() {
-  Serial.println("< calibration parameters >");
-  Serial.println("accel bias [g]: ");
-  Serial.print(mpu.getAccBiasX() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
-  Serial.print(", ");
-  Serial.print(mpu.getAccBiasY() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
-  Serial.print(", ");
-  Serial.print(mpu.getAccBiasZ() * 1000.f / (float)MPU9250::CALIB_ACCEL_SENSITIVITY);
-  Serial.println();
-  Serial.println("gyro bias [deg/s]: ");
-  Serial.print(mpu.getGyroBiasX() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
-  Serial.print(", ");
-  Serial.print(mpu.getGyroBiasY() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
-  Serial.print(", ");
-  Serial.print(mpu.getGyroBiasZ() / (float)MPU9250::CALIB_GYRO_SENSITIVITY);
-  Serial.println();
-  Serial.println("mag bias [mG]: ");
-  Serial.print(mpu.getMagBiasX());
-  Serial.print(", ");
-  Serial.print(mpu.getMagBiasY());
-  Serial.print(", ");
-  Serial.print(mpu.getMagBiasZ());
-  Serial.println();
-  Serial.println("mag scale []: ");
-  Serial.print(mpu.getMagScaleX());
-  Serial.print(", ");
-  Serial.print(mpu.getMagScaleY());
-  Serial.print(", ");
-  Serial.print(mpu.getMagScaleZ());
-  Serial.println();
 }
--- collect_arduino_data.py
+++ collect_arduino_data.py
@@ -1,57 +1,52 @@
 import bluetooth 
 import time
 import numpy as np
-# 24:62:AB:D2:A7:06
+
+# ESP32 Metadata
 bd_addr = "24:62:AB:D2:A7:06"
 port = 1
+
+# Establish BLE Socket to communicate between computer and ESP32 board
 sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((bd_addr, port))
 
+delimiter = '|'
 
-# sock.bind(bd_addr)
-# sock.listen(1)
-
-# server_sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-# server_sock.bind(("24:62:AB:D2:A7:06", port))
-# server_sock.listen(1)
-# client_sock, address = server_sock.accept()
-split = 0
-held = 0
-delimiter = '-'
-
+# Communication loop
 while True: 
+    # # Send data from computer to ESP32:
     # data = "hello!\n"
     # sock.send(data.encode())
+
+    # param = buffer width, # Bytes to receive over socket
     receive_data = sock.recv(4096)
+
+    # decode received data into interpretable data type (string)
     decoded_data = receive_data.decode()
-    split_data = np.array(decoded_data.strip().split(delimiter))[1:]
+    
+    # strip string and split by specified delimiter; ignore the first and last elements to prevent
+    # edge behavior with sending data over sockets.
+    #
+    # Example: "3.52" may get sent as "3." and "52." Because we use timer.sleep(0.25), our computer
+    # receives slower than the ESP32 sends, resulting in a large "packet" containing multiple sensor readings.
+    # In our testing with this method, the "3." would always be the final value in a packet, and the "52" would
+    # always be the first value in a packet, meaning we can deal with this by ignoring those values.
+    # Due to the speed at which we are sending sensor data, missing a single data point is not significant,
+    # and we need to perform processing regardless, meaning the precision of the raw data does not matter.
+    split_data = np.array(decoded_data.strip().split(delimiter))[1:-1] 
+
+    # Remove any remaining null data
     split_data = split_data[split_data != '']
-    # print(split_data)
+
+    # Parse characters as floating-point values
     float_data = split_data.astype(float)
-    # print(float_data)
+
+    # As a sanity check, verify that the data array is not empty -- then, perform processing:
     if (len(float_data) != 0):
         print(np.round(np.mean(float_data), 2))
     time.sleep(0.25)
-    # time.sleep(1)
-    # print(type(decoded_data))
-
-    # if '.' not in decoded_data:
-    #     if split == 0:
-    #         print(decoded_data)
-    #     elif split == 1:
-    #         print(held + decoded_data)
-    #         split = 0
-    #         held = 0
-    # else:
-    #     split = 1
-    #     held = decoded_data
  
     # print(receive_data.decode())
-    # # print(ord(receive_data.decode()))
-    # decode_data = receive_data.decode().strip().split()
-    # print(decode_data)
-    # print(ord(decode_data[0]))
-    # time.sleep(0.5);
-    # int_data = int(decode_data)
-    # print(int_data)
+    # print(ord(receive_data.decode()))
+
 # sock.close()
